3870f4dd2a659fe4a931bbdf55558bfe8bf84041
Thinprep <blumming@icloud.com>
2025-03-11 14:11:18 +0900

    initial

---
+++ first_week.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 11 14:07:07 2025
+
+@author: cic
+"""
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# %%
+
+print("Hello world")
+print("!!!")
+
+a=100
+b= a+2
+
+c=[1,2,4,6,8,11]
+
+
+
+
---
+++ second_week.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 11 14:07:37 2025
+
+@author: cic
+"""
+a=200
+if a<100:
+    print("100보다 작군요")
+else :
+    print("100보다 크군요")
